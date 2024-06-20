ef590ed72b8be6a433feb85f0da82fb3a9317fca
max-kay <max-kay@users.noreply.github.com>
2024-06-20 13:55:58 +0000

    deploy: d351e4d14dcc8dc1be789fad0ae5d1e3f8c9a047

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ eval("__webpack_require__.a(module, async (__webpack_handle_async_dependencies__
   \******************************/
 /***/ ((module, exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.v(exports, module.id, \"77eae88864bea41afa79\");\n\n//# sourceURL=webpack:///../pkg/website_bg.wasm?");
+eval("module.exports = __webpack_require__.v(exports, module.id, \"61dbacc3ff947964ad17\");\n\n//# sourceURL=webpack:///../pkg/website_bg.wasm?");
 
 /***/ })
 
